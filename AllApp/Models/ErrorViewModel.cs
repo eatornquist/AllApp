3ec0dd102c4b8b3c8db150e4d4e4eeb2fa6d7288
Eduardo Tornquist <eatornquist04@gmail.com>
2023-07-14 05:54:22 -0400

    Add project files.

---
+++ AllApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AllApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
